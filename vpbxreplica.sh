8b7d22fb9cfe82e658b3bcbc7a3bc7a94e96f08b
rodrigocuadra <30705964+rodrigocuadra@users.noreply.github.com>
2021-04-26 06:13:57 -0600

    Update vpbxreplica.sh
    Add Asterisk Database Replica

--- vpbxreplica.sh
+++ vpbxreplica.sh
@@ -525,6 +525,21 @@ mysql -uroot -e "START SLAVE;"
 echo -e "*** Done Step 5 ***"
 echo -e "5"	> step.txt
 
+cat > /usr/local/bin/vpbxstart << EOF
+#!/bin/bash
+# This code is the property of VitalPBX LLC Company
+# License: Proprietary
+# Date: 25-Apr-2021
+# Stop Asterisk, copy asterisk database and start Asterisk again
+#
+systemctl stop asterisk
+/bin/cp -Rf /home/sync/var/spool/asterisk/sqlite3_temp/astdb.sqlite3 /var/lib/asterisk/astdb.sqlite3
+systemctl start asterisk
+EOF
+chmod +x /usr/local/bin/vpbxstart
+scp /usr/local/bin/vpbxstart root@$ip_standby:/usr/local/bin/vpbxstart
+ssh root@$ip_standby 'chmod +x /usr/local/bin/vpbxstart'
+
 vitalpbx_cluster_ok:
 echo -e "************************************************************"
 echo -e "*                VitalPBX Replica OK                       *"
