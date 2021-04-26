6841d40e828db6c98abc3b3775aad75fe754852a
rodrigocuadra <30705964+rodrigocuadra@users.noreply.github.com>
2021-04-26 06:14:17 -0600

    Delete vpbxstart.sh

--- vpbxstart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# This code is the property of VitalPBX LLC Company
-# License: Proprietary
-# Date: 25-Apr-2021
-# Stop Asterisk, copy asterisk database and start Asterisk again
-#
-systemctl stop asterisk
-/bin/cp -Rf /home/sync/var/spool/asterisk/sqlite3_temp/astdb.sqlite3 /var/lib/asterisk/astdb.sqlite3
-systemctl start asterisk
